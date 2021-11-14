4fcd7ef96e503fe530e512a83cf27f302e9bd234
Taylor Thornton <thornrae@gmail.com>
2021-11-13 18:35:21 -0800

    sharing input values & adding multiple inputs

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
 import Input from '../../shared/components/FormElements/Input.js';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators.js';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators.js';
 
 
 import './NewPlace.css';
 
 const NewPlace = () => {
+
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+
   return (
     <form className="place-form">
-      <Input 
+      <Input
+        id="title" 
         element="input"  
         type="text" 
         label="Title" 
         validators={[VALIDATOR_REQUIRE()]} 
-        errorText="please enter a valid title."/>
+        errorText="please enter a valid title."
+        onInput={titleInputHandler}
+        />
+         <Input 
+        id="description"
+        element="textarea"  
+        label="Description" 
+        validators={[VALIDATOR_MINLENGTH(5)]} 
+        errorText="please enter a valid description at least 5 characters."
+        onInput={descriptionInputHandler}
+        />
+       
     </form>
   )
 }
