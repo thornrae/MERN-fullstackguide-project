02282c1c5350b463efcf648d7d1b689937182126
Taylor Thornton <thornrae@gmail.com>
2021-11-15 17:27:58 -0800

    adjusted form hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom'
 
 import Input from '../../shared/components/FormElements/Input.js'
@@ -7,7 +7,7 @@ import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH} from '../../shared/util/validat
 import {useForm} from '../../shared/hooks/form-hook.js';
 import './PlaceForm.css';
 
-const PLACES = [
+const DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
@@ -36,22 +36,40 @@ const PLACES = [
 
 const UpdatePlace = () => {
   // console.log(useParams());
+  const [isLoading, setIsLoading] = useState(true);
+
   const placeId = useParams().placeId;
 
-  const identifiedPlace = PLACES.find(p => p.id === placeId);
-
-  const [formState, inputHandler] = useForm({
+  const [formState, inputHandler, setFormData] = useForm({
     title: {
-      value: identifiedPlace.title,
-      isValid: true
+      value: '',
+      isValid: false
     }, 
     description: {
-      value: identifiedPlace.description, 
-      isValid: true
+      value: '', 
+      isValid: false
     }
   }, 
-    true
+    false
   );
+
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  useEffect(() => {
+    setFormData({
+      title: {
+        value: identifiedPlace.title,
+        isValid: true
+      }, 
+      description: {
+        value: identifiedPlace.description, 
+        isValid: true
+      }
+    }, true);
+    setIsLoading(false)
+  }, [setFormData, identifiedPlace])
+
+  
 
   const placeUpdateSubmitHandler = event => {
     event.preventDefault();
@@ -65,8 +83,15 @@ const UpdatePlace = () => {
       </div>
     )
   }
-  return (
 
+  if(isLoading) {
+    return (
+      <div className="center"> 
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+  return (
     <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input 
         id="title" 
@@ -92,8 +117,6 @@ const UpdatePlace = () => {
       <Button type="submit" disabled={!formState.isValid}>Update Place</Button>
     </form>
 )
-  
-
 }
 
 export default UpdatePlace;
