dff0c073d4c85fdac12e015f1e0ce0212df23c6a
Taylor Thornton <thornrae@gmail.com>
2021-12-09 19:20:51 -0800

    POST request from frontend to create new place connected to logged in user complete

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -55,7 +55,7 @@ const Auth = () => {
 
     if(isLoginMode) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/login', 
           'POST',
            JSON.stringify({
@@ -67,7 +67,7 @@ const Auth = () => {
            },
            
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch(err){
         //okay to be empty. or do .then
       }
@@ -76,7 +76,7 @@ const Auth = () => {
           // console.log(responseData)
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup', 
           'POST', 
           JSON.stringify({
@@ -88,7 +88,7 @@ const Auth = () => {
           'Content-Type':'application/json',
         }
       );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
   }
 };
