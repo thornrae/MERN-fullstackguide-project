91561788f3b0c7f38ff986869c2122820e291d05
Taylor Thornton <thornrae@gmail.com>
2021-12-27 10:50:57 -0800

    auth functionality: automatically log user in & automatically log a user out. Added time expiration to token

--- src/App.js
+++ src/App.js
@@ -8,15 +8,20 @@ import Auth from './user/pages/Auth.js';
 import MainNavigation from './shared/components/Navigation/MainNavigation.js';
 import {AuthContext} from './shared/context/auth-context.js';
 
+let logoutTimer;
+
 function App() {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState()
   const [userId, setUserId] = useState(false);
 
   const login = useCallback( (uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60 );
+    const tokenExpirationDate = 
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60 );
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
       'userData', 
       JSON.stringify({
@@ -27,6 +32,7 @@ function App() {
       )
   }, [])
 
+
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'));
     if(storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
@@ -36,9 +42,19 @@ function App() {
 
   const logout = useCallback( () => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
     localStorage.removeItem('userData');
   }, [])
+
+  useEffect(() => {
+    if(token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  },[token, logout, tokenExpirationDate ])
 
   let routes;
 
